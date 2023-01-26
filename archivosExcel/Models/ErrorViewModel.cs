0aa93a079f47890d3d402a8556cfa05e7301be76
ocastelblack <oscard.castelblanco@gmail.com>
2023-01-26 11:53:57 -0500

    Agregar archivos de proyecto.

---
+++ archivosExcel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace archivosExcel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ archivosExcel/Models/ViewModels/VMContacto.cs
@@ -0,0 +1,13 @@
+﻿namespace archivosExcel.Models.ViewModels
+{
+    public class VMContacto
+    {
+        public string nombre { get; set; }
+
+        public string Apellido { get;set; }
+
+        public string direccion { get; set; }
+
+        public string Fecha_nacimiento { get; set; }
+    }
+}
