22764ae9e90f2dfaf805263006a3aaafccb91bde
ocastelblack <oscard.castelblanco@gmail.com>
2023-01-26 12:17:34 -0500

    a

--- archivosExcel/Models/Contacto.cs
+++ archivosExcel/Models/Contacto.cs
@@ -8,7 +8,7 @@ namespace ProyectoExcel.Models
         public int IdContacto { get; set; }
         public string? Nombre { get; set; }
         public string? Apellido { get; set; }
-        public string? Telefono { get; set; }
-        public string? Correo { get; set; }
+        public string? direccion { get; set; }
+        public string? Fecha_nacimiento { get; set; }
     }
 }
